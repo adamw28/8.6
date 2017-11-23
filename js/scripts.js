89fb71d80d9e74aff2f8e951f5e118b445027aba
Adam Więcek <adamw28@gmail.com>
2017-11-23 22:30:26 +0100

    Update project

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var a=5,b=5,value=(a * a) - (2 * a * b) + (b * b);
+console.log(value);
+if(value>0){console.log("wynik dodatni");}
+	else if(value<0){console.log("wynik ujemny");}
+		else if(value==0){console.log("wynik równy 0");}
