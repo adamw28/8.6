2435f01f35600e52bb46c1674ee84de6aacc2de0
Adam Więcek <adamw28@gmail.com>
2017-11-24 20:51:08 +0100

    Correct js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 var a=5,b=5,value=(a * a) - (2 * a * b) + (b * b);
 console.log(value);
-if(value>0){console.log("wynik dodatni");}
-	else if(value<0){console.log("wynik ujemny");}
-		else if(value==0){console.log("wynik równy 0");}
+if(value>0){
+	console.log("wynik dodatni");
+} else if(value<0){
+	console.log("wynik ujemny");
+} else if(value==0){
+	console.log("wynik równy 0");
+}
